f2110880a03aff9c683c07fcbe2d7d0ec3efbfa3
Lucy Welsh <lwelsh5@outlook.com>
2022-08-17 15:23:36 +0100

    redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import 'tachyons';
+
+
 import App from './containers/App';
-import 'tachyons'; 
+import registerServiceWorker from './registerServiceWorker.js';
+import { requestRobots, searchRobots } from './containers/reducers'
 
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger() 
 
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
